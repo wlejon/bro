46c6488caa875cfa5c83c4cbb74b0e96ce4f6d5f
Jonny Brannum <jonny@novahorizons.io>
2026-04-09 00:43:29 -0500

    deleted old tests

--- apps/scene3d-demo/test.js
+++
@@ -1,8 +0,0 @@
-// Render demo 1 after several frames to verify no paint accumulation
-switchDemo(0);
-for (var i = 0; i < 10; i++) {
-    advanceTime(16);
-}
-flush();
-screenshot('scene3d-final.png');
-console.log('final screenshot saved');
