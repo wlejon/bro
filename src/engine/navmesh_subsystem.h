99150c992af37d549e827d11bc500b8b7f856e3e
wlejon <jonny.brannum@gmail.com>
2026-08-13 13:56:09 -0500

    build: gate navmesh and brogameagent for minimal profile

--- src/engine/navmesh_subsystem.h
+++ src/engine/navmesh_subsystem.h
@@ -2,6 +2,8 @@
 
 #include <memory>
 
+#if BRO_WITH_GAMEAI
+
 namespace brogameagent { class NavMesh; }
 
 namespace bro::engine {
@@ -10,3 +12,16 @@ void registerNavMeshForPump(const std::shared_ptr<brogameagent::NavMesh>& mesh);
 void pumpNavMeshObstacles(float dt);
 
 } // namespace bro::engine
+
+#else
+
+namespace brogameagent { class NavMesh; }
+
+namespace bro::engine {
+
+inline void registerNavMeshForPump(const std::shared_ptr<brogameagent::NavMesh>&) {}
+inline void pumpNavMeshObstacles(float) {}
+
+} // namespace bro::engine
+
+#endif
--- src/engine/scene_audio_sync.h
+++ src/engine/scene_audio_sync.h
@@ -1,7 +1,18 @@
 #pragma once
 
 #include <memory>
+
+#if BRO_WITH_3D
 #include "scene/scene_graph.h"
+#else
+namespace bro::scene {
+class SceneNode;
+class SceneGraph {
+public:
+    struct LivenessToken {};
+};
+}
+#endif
 
 namespace broaudio { class Engine; }
 
@@ -13,11 +24,11 @@ public:
     static void shutdown();
     static void sync(float dtSec);
 
-    static void attachAudioEmitter(std::weak_ptr<scene::SceneGraph::LivenessToken> token,
-                                   scene::SceneNode* node, int handle, bool isVoice);
+    static void attachAudioEmitter(std::weak_ptr<bro::scene::SceneGraph::LivenessToken> token,
+                                   bro::scene::SceneNode* node, int handle, bool isVoice);
     static void detachAudioEmitter(uint32_t nodeId);
-    static void bindAudioListenerToCamera(std::weak_ptr<scene::SceneGraph::LivenessToken> token,
-                                         scene::SceneGraph* graph, bool enable);
+    static void bindAudioListenerToCamera(std::weak_ptr<bro::scene::SceneGraph::LivenessToken> token,
+                                         bro::scene::SceneGraph* graph, bool enable);
 };
 
 } // namespace bro::engine
